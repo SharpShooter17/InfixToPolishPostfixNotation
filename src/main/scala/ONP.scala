2383047a5561f8436972866131130bf31655724f
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2020-12-01 15:43:58 +0100

    Evaluate onp

--- src/main/scala/ONP.scala
+++ src/main/scala/ONP.scala
@@ -9,30 +9,54 @@ object ONP extends App {
 
   private val input = StdIn.readLine()
   private val tokens = findTokens(input)
-  private val output = convertToOnp(tokens)
+  private val onpTokens = convertToOnp(tokens)
+  private val calculatedValue = calculate(onpTokens);
 
-  println(s"Output: ${output.mkString(" ")}")
+  println(s"ONP: ${onpTokens.mkString(" ")}")
+  println(s"Evaluated value: $calculatedValue")
+
+  def calculate(tokens: List[String]) = {
+    val stack = new mutable.Stack[Double]()
+    tokens.foreach {
+      case "+" =>
+        stack.push(stack.pop + stack.pop)
+      case "-" =>
+        val x = stack.pop
+        stack.push(stack.pop - x)
+      case "*" =>
+        stack.push(stack.pop * stack.pop)
+      case "/" =>
+        val x = stack.pop
+        stack.push(stack.pop / x)
+      case "^" =>
+        val x = stack.pop
+        stack.push(math.pow(stack.pop, x))
+      case token =>
+        stack.push(token.toDouble);
+    }
+    stack.pop()
+  }
+
+
 
   private def convertToOnp(tokens: List[String]): List[String] = {
     val stack = new mutable.Stack[String]()
     val onp = new ListBuffer[String]()
 
-    tokens.foreach { token =>
-      token match {
-        case "^" | "*" | "/" | "+" | "-" =>
-          while (stack.nonEmpty && prior(stack.top) >= prior(token)) {
-            onp.addOne(stack.pop())
-          }
-          stack.push(token)
-        case "(" => stack.push(token)
-        case ")" =>
-          var top: String = stack.pop()
-          while (top != "(") {
-            onp.addOne(top)
-            top = stack.pop()
-          }
-        case _ => onp.addOne(token)
-      }
+    tokens.foreach {
+      case token@("^" | "*" | "/" | "+" | "-") =>
+        while (stack.nonEmpty && prior(stack.top) >= prior(token)) {
+          onp.addOne(stack.pop())
+        }
+        stack.push(token)
+      case token@"(" => stack.push(token)
+      case ")" =>
+        var top: String = stack.pop()
+        while (top != "(") {
+          onp.addOne(top)
+          top = stack.pop()
+        }
+      case token => onp.addOne(token)
     }
 
     while (stack.nonEmpty) {
